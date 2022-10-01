b3ccc35e2ed4e3dce5c511428358f24a1d616049
c97Ernesto <ernesto-cafre@outlook.com>
2022-10-01 16:47:34 -0300

    ejercicio18

---
+++ Practica3_ShellScripting/ejercicio18.sh
@@ -0,0 +1,24 @@
+#Crear un script que verifique cada 10 segundos si un usuario se ha
+#logueado en el sistema (el nombre de usuario será pasado por parámetro).
+#Cuando el usuario finalmente se loguee, el programa deberá mostrar un
+#mensaje "Usuario XXXX logueado en el sistema", y salir.
+
+#Verificamos el parámetro
+if [ $# -ne 1 ]; then
+	echo "Ingresar parámetro: nombreUsuario."
+	exit 1
+	
+#Verificamos que existe usuario
+elif [ $1 != "$(cat /etc/passwd | cut -d ":" -f 1 | grep $1)" ]; then
+	echo "El usuario ingresado no existe."
+	exit 2
+	
+#Buscamos que ingrese
+else
+	while [ $1 != "$(who | cut -d" " -f 1)" ]; do
+		echo "Desconectado"
+		sleep 5
+	done
+	echo "Usuario $1 logueado en el sistema"
+fi
+exit 0
