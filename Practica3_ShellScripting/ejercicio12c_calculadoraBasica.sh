a3d0a782860a8527ff97abf37a6bf994c28b2d77
c97Ernesto <ernesto-cafre@outlook.com>
2022-10-07 22:19:36 -0300

    ej12c 'arreglo'

--- Practica3_ShellScripting/ejercicio12c_calculadoraBasica.sh
+++ Practica3_ShellScripting/ejercicio12c_calculadoraBasica.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ $# -ne 3 ]; then
-	echo "Cantidad de parámetros incorrectos: "
+	echo "Cantidad de parámetros incorrectos (* entre comillas ('*')): "
 	echo "operando_1 operador operando_2"
 	exit 1
 else
-	echo "Resultado: $(expr $1 $2 $3)"
+	echo "Resultado: $[ ${1} ${2} ${3} ]"
 fi
 exit 0
