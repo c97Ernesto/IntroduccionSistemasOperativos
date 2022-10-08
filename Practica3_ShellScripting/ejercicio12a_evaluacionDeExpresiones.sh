914caf3b1c35e7f4d6681b5cd730e059dcea8413
c97Ernesto <ernesto-cafre@outlook.com>
2022-10-07 22:06:45 -0300

    ej12a arreglo multiplicación

--- Practica3_ShellScripting/ejercicio12a_evaluacionDeExpresiones.sh
+++ Practica3_ShellScripting/ejercicio12a_evaluacionDeExpresiones.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Ingresar dos números: "
+#expresiones escritas de distintas maneras, funcionan de igual forma
+#La multiplicación así $(expr ${n1} * ${n2}) no funciona
+
+echo "Ingresar dos números (separados por espacio): "
 read n1 n2
-echo "Multiplicación: buscar como se pone la barra invertida no me funciona."
-echo "Suma: $(expr ${n1} + ${n2})"
-echo "Resta: $(expr ${n1} - ${n2})"
+echo "Multiplicación: $[ n1 * n2 ]"
+echo "Suma: $(expr $n1 + $n2)"
+echo "Resta: $[n1 - ${n2}]"
 echo "Division: $(expr ${n1} / ${n2})"
 echo "El mayor es: "
 if [ ${n1} -gt ${n2} ]
