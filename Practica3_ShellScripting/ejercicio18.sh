e54bca0994b2da4bb552ea6332f8a9880cff3954
c97Ernesto <ernesto-cafre@outlook.com>
2022-10-11 18:33:17 -0300

    ej18.sh, detallando

--- Practica3_ShellScripting/ejercicio18.sh
+++ Practica3_ShellScripting/ejercicio18.sh
@@ -9,11 +9,15 @@ if [ $# -ne 1 ]; then
 	exit 1
 	
 #Verificamos que existe usuario
+#/etc/passwd contiene información sobre los usuarios del sistema: nombre 
+#usuario, id de usuario, id grupo, descripción usuario, su home e intérprete 
+#de comandos por defecto
 elif [ $1 != "$(cat /etc/passwd | cut -d ":" -f 1 | grep $1)" ]; then
 	echo "El usuario ingresado no existe."
 	exit 2
 	
 #Buscamos que ingrese
+#cut <-d(delimitador) <"caracter">> <-f(campo) <nro de campo>> <archivo>
 else
 	while [ $1 != "$(who | cut -d" " -f 1)" ]; do
 		echo "Desconectado"
