a1279a9a9a99bb4e6b461761bd9779e685a731a9
c97Ernesto <ernesto-cafre@outlook.com>
2023-05-23 16:51:40 -0300

    ejerParcial01

--- Practica3_ShellScripting/ejercicioParcial01.sh
+++ Practica3_ShellScripting/ejercicioParcial01.sh
@@ -5,36 +5,45 @@
 # -Si el parámetro es un archivo existente en el FileSystem deberá comprimirlo 
 # dejando el archivo comprimido en lugar del original.
 # -Si el parámetro es un directorio existente en el FileSystem:
-#	-si tiene permiso de Lectura deberá empaquetarlo y comprimirlo.	
-#	-si tiene permiso de Escritura deberá eliminarlo junto con todo su contenido.
+#       -si tiene permiso de Lectura deberá empaquetarlo y comprimirlo. 
+#       -si tiene permiso de Escritura deberá eliminarlo junto con todo su contenido.
 # -Si el parámetro no existe, deberá contarlo e indicar la cantidad total al 
 # finalizar el script.
 
-if [ $# -eq 0 ]; then
-	echo "Se debe recibir al menos un parámetro (ruta absoluta de archivo)."
+if [ $# -lt 1 ]; then
+	echo "Al menos se debe recibir un parámetro."
 	exit 1
-
-else
-	for i in $*; do
-		if [ -f $i ]; then
-			echo "$i: es archivo, comprimiendo $i"
-			7z -r $i.7z $i
-			
-		elif [ -d $i ]; then
-			echo "$i Es directorio..."
-			
-			if [ -w $i ]; then
-				echo "con permisos de Escritura, eliminando $i ."
-				rm -r $i
-				
-			elif [ -r $i ]; then
-				echo "con permisos de Lectura, empaquetando y comprimiendo $i ."
-				tar -zvcf /$HOME/CompresionYEmpaquetado.tgz $i
-				
-			fi
-		else
-			echo "$i: no es archivo ni directorio."
-		fi
-	done
 fi
 
+rutas=("$@")
+
+for i in "${rutas[@]}"; do
+
+	if [ -e "$i" ]; then
+		echo -n "La ruta ingresada como parámetro "$i" existe. "
+
+		if [ -f "$i" ]; then
+			echo "Es archivo, se comprimirá."
+			#gzip: comando para comprimir archivo regular y generar un nuevo
+			#archivo "archivo.extension.gz" en el mismo directorio, reemplazando
+			#el archivo original
+			gzip "$i"
+		elif [ -d "$i" ]; then
+			echo -n "Es un directorio "
+			if [ -r "$i" ]; then
+				echo "con permiso de lectura, se empaquetará y comprimirá."
+				tar czvf "${i}.tar.gz" "$i"/
+			elif [ -w "$i" ]; then
+				echo "con permiso de escritura, se eliminará con todo su contenido."
+				rm -r "$i"
+			fi
+		fi
+	else
+		echo -n "No existe la ruta proporcionada como paŕametro "
+		echo "$i"
+		#((noRutas+=1))
+		noRutas=$((noRutas + 1))
+	fi
+done
+echo "Rutas inexistentes: $noRutas"
+exit 0
