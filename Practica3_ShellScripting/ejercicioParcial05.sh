37e2db30c0df739d78dea6e505840f70df721237
c97Ernesto <ernesto-cafre@outlook.com>
2023-05-24 12:30:48 -0300

    ejerciciParcial05

--- Practica3_ShellScripting/ejercicioParcial05.sh
+++ Practica3_ShellScripting/ejercicioParcial05.sh
@@ -1,96 +1,104 @@
+#!bin/bash
 #Realice un script que agregue todos los nombres de usuario del sistema a un arreglo
 #e implemente las siguientes funciones:
-#	*existe <parámetro1>: Devuelve 0 si el usuario existe en el arreglo, 1 en caso
-#	contrario
-#	*eliminar_usuario <parámetro1>: Si el usuario pasado como parámetro existe en el
-#	arreglo, lo elimina del mismo. Caso contrario devuelve código de error 2.
-#	*usuarios_con_patron <parámetro1>: Recorre el arreglo e imprime en pantalla 
-#	los nombres de los usuarios en cuyo caracteres aparece el patrón pasado como
-#	parámetro.
-#	*cantidad:Imprime la cantidad total de ususarios en el arreglo.
-#	*usuarios: imprime todos los nombres de los usuarios que están en el arreglo
+#       *existe <parámetro1>: Devuelve 0 si el usuario existe en el arreglo, 1 en caso
+#       contrario
+#       *eliminar_usuario <parámetro1>: Si el usuario pasado como parámetro existe en el
+#       arreglo, lo elimina del mismo. Caso contrario devuelve código de error 2.
+#       *usuarios_con_patron <parámetro1>: Recorre el arreglo e imprime en pantalla 
+#       los nombres de los usuarios en cuyo caracteres aparece el patrón pasado como
+#       parámetro.
 
-usuariosDelSistema=$(cat /etc/passwd | cut -d":" -f1) # = cut -d ":" -f1 /etc/passwd
-nombres=($usuariosDelSistema)
+usuarios=()
 
-existe () {
-	if [ $# -ne 1 ]; then	    #siempre validar parámetros(especialmente en parcial)
-		echo "Solo un parámetro."
-		return 255
-	else
-		for nombre in ${nombres[*]}; do	
-			if [ $1 == $nombre ]; then
-				break 
-			fi
-		done
-	
-		if [ $1 == $nombre ]; then
-			echo "El usuario $1 existe."
-			return 0
-		else
-			echo "El usuario $1 no existe"
-		fi
+for i in $(cat /etc/passwd | cut -d":" -f1); do
+#	echo "Elemento $((j+=1)): $i"
+	usuarios[${#usuarios[@]}]="$i"
+done
+
+function existe {
+	if [ $# -ne 1 ]; then
+		echo "Se debe ingresar el usuario como parámetro."
+		return 2
 	fi
-}
 
-eliminarUsuario(){
-	if [ $# -ne 1 ]; then	    
-		echo "Solo un parámetro."
-		return 255
-	else
-	
-		for ((i=0; i<${#nombres[*]}; i++)); do	
-			if [ $1 == ${nombres[i]} ]; then
-				break 
-			fi
-		done
-		
-		if [ "$1" == "${nombres[i]}" ]; then
-			unset nombres[$i]		#dejamos la posicion vacía
-			nombres=(${nombres[*]})		#seteamos el arreglo con los elementos del arreglo
-			echo "Usuario $1 eliminado."
-			return 0
-		else
-			echo "El usuario "$1" no xiste"
-			return 1
+	local ok=""
+	for i in "${usuarios[@]}"; do
+		if [ "$1" = "$i" ]; then
+			ok="no vacía"
+			break
 		fi
+	done
+	#z devulve 0 si está vacío <=> !-n
+	if [ -z "$ok" ]; then
+		echo "El usuario no existe en el arreglo"
+		return 1
 	fi
+	echo "El usuario "$1" existe en el arreglo"
+	return 0
 }
 
-cantidad(){
-	echo "La cantidad de elementos del arreglo es: ${#nombres[*]}"
-}
-
-usuarios(){
-	echo "Los usuarios son: ${nombres[*]}"
-}
-
-usuarioConPatron(){
-	if [ $# -ne 1 ]; then	    
-		echo "Solo un parámetro."
-		return 255
-	else
-
-		for nombre in ${nombres[*]}; do	
-#			echo $nombre
-			if [ "$(echo $nombre | grep $1)" = "$nombre" ]; then
-				break 
-			fi
-		done
-		
-		if [ "$(echo $nombre | grep $1)" = "$nombre" ]; then
-			echo "$nombre tiene el patrón $1"		 
-		else
-			echo "No hay ningún usuario con ese patrón"
+function eliminar {
+	#Se valida previamente si existe el usuario
+	local posicion=0
+	for i in "${usuarios[@]}"; do
+		if [ "$1" = "$i" ]; then
+			break
 		fi
-		
-	fi
+		((posicion++))
+	done
+
+	unset usuarios[$posicion]
+	usuarios=(${usuarios[@]})
+	return 0
 }
 
-#Implementar funciones
-existe mail
-cantidad
-eliminarUsuario mail
-usuarioConPatron daem
-cantidad
-usuarios
+function usuarios_con_patron {
+	if [ $# -ne 1 ]; then
+		echo "Se debe recibir un patrón como parámetro."
+		return 1
+	fi
+
+	local patron="$1" 
+	for i in "${usuarios[@]}"; do
+		#n devuelve 0 si no está vacío <=> !-z
+		if [ -n "$(echo "$i" | grep "$patron")" ]; then
+			echo "$i"
+		fi
+
+	done
+	return 0
+}
+
+
+opciones=("Buscar usuario" "Eliminar usuario del arreglo" "Buscar usuarios que coincidan con patrón"  "Salir")
+
+select opcion in "${opciones[@]}"; do
+
+	case $opcion in
+		"Buscar usuario")
+			echo -n "Ingresar nombre de usuario: "
+			read nombre
+			existe "$nombre"
+		;;
+		"Eliminar usuario del arreglo")
+			echo -n "Ingresar nombre de usuario: "
+			read nombre
+			existe "$nombre"
+			if [ $? -eq 0 ]; then
+				eliminar "$nombre"
+			fi
+		;;
+		"Buscar usuarios que coincidan con patrón")
+			echo -n "Ingresar patrón: "
+			read patron
+			usuarios_con_patron "$patron"
+		;;
+		"Salir")
+			break
+		;;
+		*)
+			echo "Opción incorrecta."
+	esac
+done
+exit 0
